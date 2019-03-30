6b9fef48a74f24d200cd009a25f23a3899df2a11
cylee0909 <cylee0909@qq.com>
2019-03-30 22:27:20 +0800

    add page size call

--- test.c
+++ test.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 int main(void) {
+    static_test();
     pid_t pid = getpid();
     printf("mypid = %d \n", pid);
+    static_test();
+
+    extern char etext, edata, end;
+    printf("%d, %c, %c\n", etext, edata, end);
+
+    int page_size = sysconf(_SC_PAGESIZE);
+    printf("page size = %ld\n", page_size);
     return 0;
+}
+
+int static_test() {
+    static int i;
+    i ++;
+    printf("i is %d\n", i);
 }
