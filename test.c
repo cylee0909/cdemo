74b917206cc5a9ff12887a0fbd78082e5b62b077
cylee0909 <cylee0909@qq.com>
2019-04-01 22:23:57 +0800

    add some sys call

--- test.c
+++ test.c
@@ -2,6 +2,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <gnu/libc-version.h>
+#include <errno.h>
+#include <string.h>
+
 int main(void) {
     static_test();
     pid_t pid = getpid();
@@ -13,6 +17,14 @@ int main(void) {
 
     int page_size = sysconf(_SC_PAGESIZE);
     printf("page size = %ld\n", page_size);
+    
+    char* libc_v = gnu_get_libc_version();
+    printf("libc version is %s\n", libc_v);
+    printf("error no is %d\n", errno);
+    errno = EBFONT;
+    perror("an error occured");
+    printf("error msg = %s\n", strerror(errno));
+
     return 0;
 }
 
